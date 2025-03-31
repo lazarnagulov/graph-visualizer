9bcda33e6d6f09035c231de848af2b1d3b5ef8b2
Lazar Nagulov <lazarnagulov1@gmail.com>
2025-03-31 18:55:30 +0200

    #7 feat: Add node model

---
+++ api/visualizer/api/model/node.py
@@ -0,0 +1,45 @@
+from typing import Dict
+
+from typing_extensions import Optional
+
+
+class Node:
+
+    __slots__ = ['__properties']
+
+    def __init__(self, **properties) -> None:
+        self.__properties: Dict[str, any] = properties
+
+    @property
+    def properties(self) -> Dict[str, any]:
+        return self.__properties
+
+    @properties.setter
+    def properties(self, attributes: Dict[str, any]) -> None:
+        if not isinstance(attributes, dict):
+            raise TypeError(f"Error: expected dict but got { type(attributes) }")
+        self.__properties = attributes
+
+    def add_property(self, key: str, value: any) -> None:
+        if not isinstance(key, str):
+            raise TypeError(f"Error: expected str but got { type(key) }")
+        self.__properties[key] = value
+
+    def remove_property(self, key: str) -> Optional[any]:
+        if not isinstance(key, str):
+            raise TypeError(f"Error: expected str but got { type(key) }")
+        return self.__properties.pop(key, None)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Node):
+            return False
+        if self.properties.keys() == other.properties.keys():
+            return False
+        return all(self.properties[k] == other.properties[k] for k in self.properties)
+
+    def __hash__(self) -> int:
+        return hash(id(self))
+
+    def __str__(self) -> str:
+        properties_str = ', '.join(f'{key}: {value}' for key, value in self.__properties.items())
+        return f'Node({ properties_str })'
