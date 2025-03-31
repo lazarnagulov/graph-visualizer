de0c1d8fa8f8ba8e37065deaf0d8c8e6c10ee188
Lazar Nagulov <lazarnagulov1@gmail.com>
2025-03-31 18:48:13 +0200

    #7 feat: Add plugin interfaces

---
+++ api/visualizer/api/service/visualizer_plugin.py
@@ -0,0 +1,24 @@
+from abc import abstractmethod
+from typing import Tuple
+
+from ..model.graph import Graph
+from ..service.plugin import Plugin
+
+
+class VisualizerPlugin(Plugin):
+    """
+    An abstraction representing a plugin for visualising graph data.
+    """
+    @abstractmethod
+    def visualize(self, graph: Graph, **kwargs) -> Tuple[str, str]:
+        """
+        Visualizes the given graph.
+
+        :param graph: The graph to visualize.
+        :param kwargs: Arbitrary keyword arguments for customization (e.g., layout options).
+        :type graph: Graph
+        :type kwargs: any
+        :return: A tuple containing the HTML header and body for the visualization.
+        :rtype: Tuple[str, str]
+        """
+        ...
