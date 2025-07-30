ab4d954275035433f4c2212259b42b63e6f3aa7e
Lazar Nagulov <lazarnagulov1@gmail.com>
2025-07-30 10:48:51 +0200

    #11 feat: Notify that graph is changed with command

--- core/visualizer/core/usecase/command/edge/create_edge_command.py
+++ core/visualizer/core/usecase/command/edge/create_edge_command.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 
 from visualizer.core.usecase.command import Command
 
@@ -6,19 +6,31 @@ from visualizer.api.model.graph import Graph
 from visualizer.api.model.node import Node
 from visualizer.api.model.edge import Edge
 
+from visualizer.core.usecase.event_bus import EventBus
+
 
 class CreateEdgeCommand(Command):
 
-    __slots__ = ["__graph", "__source", "__destination", "__properties"]
+    __slots__ = ["__graph", "__source", "__destination", "__properties", "__event_bus"]
 
-    def __init__(self, graph: Graph, source: Node, destination: Node, properties: Dict[str, Any]) -> None:
+    def __init__(
+        self,
+        graph: Graph,
+        source: Node,
+        destination: Node,
+        properties: Dict[str, Any],
+        event_bus: Optional[EventBus] = None
+    ) -> None:
         self.__graph: Graph = graph
         self.__source: Node = source
         self.__destination: Node = destination
         self.__properties: Dict[str, Any] = properties
+        self.__event_bus = event_bus
 
     def execute(self) -> None:
         self.__graph.insert_edge(Edge(self.__source, self.__destination, **self.__properties))
+        if self.__event_bus:
+            self.__event_bus.emit("graph_updated", self.__graph)
 
     def undo(self) -> None:
         raise NotImplemented("implement create edge command undo")
--- core/visualizer/core/usecase/command/node/create_node_command.py
+++ core/visualizer/core/usecase/command/node/create_node_command.py
@@ -1,22 +1,33 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from visualizer.core.usecase.command import Command
 
 from visualizer.api.model.graph import Graph
 from visualizer.api.model.node import Node
 
+from visualizer.core.usecase.event_bus import EventBus
+
 
 class CreateNodeCommand(Command):
 
-    __slots__ = ["__graph, __node_id, __properties"]
+    __slots__ = ["__graph, __node_id, __properties", "__event_bus"]
 
-    def __init__(self, graph: Graph, node_id: str, properties: Dict[str, Any]) -> None:
+    def __init__(
+        self,
+        graph: Graph,
+        node_id: str,
+        properties: Dict[str, Any],
+        event_bus: Optional[EventBus]
+    ) -> None:
         self.__graph = graph
         self.__node_id = node_id
         self.__properties = properties
+        self.__event_bus = event_bus
 
     def execute(self) -> None:
         self.__graph.insert_node(Node(self.__node_id, **self.__properties))
+        if self.__event_bus:
+            self.__event_bus.emit("graph_updated", self.__graph)
 
     def undo(self) -> None:
         raise NotImplemented("implement create node undo")
