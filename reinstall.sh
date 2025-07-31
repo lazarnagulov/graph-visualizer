93586c36ae2a86409ee33be8739fcaa538cedc2c
FilipT03 <filipsombor@gmail.com>
2025-07-31 11:59:12 +0200

    #15 bugfix: Fix reinstall scripts

--- reinstall.sh
+++ reinstall.sh
@@ -4,11 +4,21 @@ if [ -f venv/bin/activate ]; then
   source venv/bin/activate
 fi
 
+if [[ "$1" == "graph_explorer" ]]; then
+    packagename="graph_visualizer_web"
+elif [[ "$1" == "core" ]]; then
+    packagename="graph_visualizer_core"
+elif [[ "$1" == "api" ]]; then
+    packagename="graph_visualizer_api"
+else
+    packagename="$1"
+fi
+
 cd "./$1"
 rm -rf ./build
-rm -rf "./$1.egg-info"
+rm -rf "./$packagename.egg-info"
 cd ../
 
-pip uninstall $1 -y
+pip uninstall "$packagename" -y
 pip install "./$1"
 
